e372a16ab305fd68e51cc04972fa1929925e36b6
kelvanince1 <incekelvan@gmail.com>
2018-04-17 10:04:28 -0500

    Add delete method handler

--- lists/src/App.js
+++ lists/src/App.js
@@ -13,18 +13,6 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
-
   nameChangedHandler = (event) => {
     this.setState( {
       persons: [
@@ -34,6 +22,16 @@ class App extends Component {
       ]
     });
   };
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+
+    this.setState({
+      persons: persons
+    })
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -56,8 +54,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
           })}
         </div>
       );
