9f091c3b2f1d60855fdcde8fc7968976cc375313
kelvanince1 <incekelvan@gmail.com>
2018-04-17 10:20:03 -0500

    Use copy of person state to modify or delete the person object

--- lists/src/App.js
+++ lists/src/App.js
@@ -5,15 +5,28 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '2114', name: 'Max', age: 28 },
+      { id: '1413', name: 'Manu', age: 29 },
+      { id: '252', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.anme = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState( {
       persons: [
         { name: 'Max', age: 28 },
@@ -56,9 +69,12 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age} />
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
           })}
         </div>
       );
