4b541b01f2d782f9cf0e3aeb9b6a7dd6276987fd
kelvanince1 <incekelvan@gmail.com>
2018-04-18 08:58:26 -0500

    Add an Error Boundary to be used at a later point

--- lists/src/App.js
+++ lists/src/App.js
@@ -4,6 +4,7 @@ import classes from './App.css';
 import Person from './Person/Person';
 import Validation from './Validation';
 import Char from './Char';
+import ErrorBoundary from './ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -92,13 +93,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                key={person.id}
                 changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
+              /></ErrorBoundary>
           })}
         </div>
       );
