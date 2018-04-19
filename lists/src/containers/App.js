432e74fc30c7c794d02fa6a7cc81d76b3b68d2e7
kelvanince1 <incekelvan@gmail.com>
2018-04-19 14:55:49 -0500

    Add a piece of state which updates using prevState

--- lists/src/containers/App.js
+++ lists/src/containers/App.js
@@ -23,7 +23,8 @@ class App extends Component {
       ],
       otherState: 'some other value',
       showPersons: false,
-      username: ''
+      username: '',
+      toggleClicked: 0
     };
   };
 
@@ -98,8 +99,12 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+
     });
   };
 
