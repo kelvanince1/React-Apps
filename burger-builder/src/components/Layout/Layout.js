9d3169694cacd5eec2e14b253313633e5c4c68bd
kelvanince1 <incekelvan@gmail.com>
2018-04-20 10:43:43 -0500

    Add LAyout styling file

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxillery';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div></div>
-    <main>
+    <main classNam={classes.Content}>
       {props.children}
     </main>
   </Aux>
---
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxillery';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Controls</div>
+      </Aux>
+    )
+  }
+}
+
+export default BurgerBuilder;
