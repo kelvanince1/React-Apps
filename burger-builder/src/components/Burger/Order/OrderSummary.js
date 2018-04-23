6d3792a375dc2f51f07edbb9c4228351c86eb92a
kelvanince1 <incekelvan@gmail.com>
2018-04-23 10:12:45 -0500

    Add price to order summary

--- burger-builder/src/components/Burger/Order/OrderSummary.js
+++ burger-builder/src/components/Burger/Order/OrderSummary.js
@@ -18,6 +18,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to checkout</p>
       <Button clicked={props.purchaseCancelled} btnType="Danger">Cancel</Button>
       <Button clicked={props.purchaseContinued} btnType="Success">Continue</Button>
