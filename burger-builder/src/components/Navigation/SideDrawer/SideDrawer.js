b392b339b3980761035cbd0bc91569cba41a804b
kelvanince1 <incekelvan@gmail.com>
2018-04-23 15:00:15 -0500

    Add logic to identify whether the backdrop should or should not be displayed to the user

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,15 +3,25 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxillery';
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.sideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.sideDrawer, classes.Open]
+  };
+
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%" margin="32px" />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <Logo height="11%" margin="32px" />
+        <nav>
+          <NavigationItems />
+        </nav>
+       </div>
+    </Aux>
   );
 };
 
