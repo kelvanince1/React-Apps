e20ec959d38eb96cc2dfa5d39e6587b935c517e4
kelvanince1 <incekelvan@gmail.com>
2018-04-24 13:32:25 -0500

    Add delete handler on the delete post button

--- blogs/src/components/FullPost/FullPost.js
+++ blogs/src/components/FullPost/FullPost.js
@@ -14,30 +14,37 @@ class FullPost extends Component {
           .then(response => {
             this.setState({
               loadedPost: response.data
-            })
-          })
-      }
-    }
+            });
+          });
+      };
+    };
+  };
 
+  deletePostHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then(res => {
+        console.log(res)
+      });
+  };
+
+  render () {
+      let post = <p style={{ texAlign: 'center' }}>Please select a Post!</p>;
+      if (this.props.id) {
+        post = <p style={{ texAlign: 'center' }}>Loading...</p>
+      }
+      if (this.state.loadedPost) {
+        post = (
+            <div className="FullPost">
+                <h1>{this.state.loadedPost.title}</h1>
+                <p>{this.state.loadedPost.body}</p>
+                <div className="Edit">
+                    <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                </div>
+            </div>
+          );
+      };
+      return post;
   }
-    render () {
-        let post = <p style={{ texAlign: 'center' }}>Please select a Post!</p>;
-        if (this.props.id) {
-          post = <p style={{ texAlign: 'center' }}>Loading...</p>
-        }
-        if (this.state.loadedPost) {
-          post = (
-              <div className="FullPost">
-                  <h1>{this.state.loadedPost.title}</h1>
-                  <p>{this.state.loadedPost.body}</p>
-                  <div className="Edit">
-                      <button className="Delete">Delete</button>
-                  </div>
-              </div>
-            );
-        };
-        return post;
-    }
 }
 
 export default FullPost;
