a6920b2622ae9900d946e16fe03de0980c247da0
kelvanince1 <incekelvan@gmail.com>
2018-04-25 17:14:18 -0500

    Add a second route to blog file

--- blogs/src/containers/Blog/Blog.js
+++ blogs/src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 
 // import axios from '../../axios';
 import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -19,6 +20,7 @@ class Blog extends Component {
                 </nav>
               </header>
               <Route path="/" exact component={Posts} />
+              <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
