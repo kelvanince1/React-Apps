899e4fc9f8508bef50d5bf78b98154a34f2f894c
kelvanince1 <incekelvan@gmail.com>
2018-04-26 13:19:26 -0500

    Add a guard to new posts link to only allow it to be shown to authd users

--- blogs/src/containers/Blog/Blog.js
+++ blogs/src/containers/Blog/Blog.js
@@ -7,30 +7,33 @@ import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-              <header>
-                <nav>
-                  <ul>
-                    <li><NavLink to="/posts" exact activeClassName="active">Posts</NavLink></li>
-                    <li><NavLink to={{
-                      pathname: '/new-post',
-                      hash: '#submit',
-                      search: '?quick-submit=true'
-                    }}>New Post</NavLink></li>
-                  </ul>
-                </nav>
-              </header>
-              <Switch>
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/posts" component={Posts} />
-                <Redirect from="/" to="/posts" />
-              </Switch>
+  state ={
+    auth: false
+  }
+  render () {
+      return (
+          <div className="Blog">
+            <header>
+              <nav>
+                <ul>
+                  <li><NavLink to="/posts" exact activeClassName="active">Posts</NavLink></li>
+                  <li><NavLink to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}>New Post</NavLink></li>
+                </ul>
+              </nav>
+            </header>
+            <Switch>
+              {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+              <Route path="/posts" component={Posts} />
+              <Redirect from="/" to="/posts" />
+            </Switch>
 
-            </div>
-        );
-    }
+          </div>
+      );
+  }
 }
 
 export default Blog;
