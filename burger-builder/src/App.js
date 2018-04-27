950608b610769354b0d8a31063fa2cb731760be6
kelvanince1 <incekelvan@gmail.com>
2018-04-27 15:51:09 -0500

    Import React Router and use the / route for the burger builder component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -7,12 +8,12 @@ import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
-      <div>
+      <Route>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
         </Layout>
-      </div>
+      </Route>
     );
   }
 }
