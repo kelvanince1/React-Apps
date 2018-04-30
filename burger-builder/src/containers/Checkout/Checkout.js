72a4a003e12b897cfa2b754e17a36009a8429ae8
kelvanince1 <incekelvan@gmail.com>
2018-04-30 07:39:46 -0500

    Fixed typo

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -15,7 +15,7 @@ class Checkout extends Component {
   componentDidMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
-    for (let param of query.entires()) {
+    for (let param of query.entries()) {
       ingredients[param[0]] = +param[1]
     }
     this.setState({
