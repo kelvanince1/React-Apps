f2d6357175d962b85b6573d55f7ea3bc25125793
kelvanince1 <incekelvan@gmail.com>
2018-05-04 08:47:36 -0500

    Reducer boiler

--- burger-builder/src/store/reducer.js
+++ burger-builder/src/store/reducer.js
@@ -3,4 +3,10 @@ import * as actionTypes from './actions';
 const initialState = {
   ingrdients: null,
   totalPrice: 4
-}
+};
+
+const reducer = (state = initialState, actionTypes) => {
+
+};
+
+export default reducer;
