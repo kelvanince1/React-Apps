84d224b552f257b336869606dc3159d977b71acb
kelvanince1 <incekelvan@gmail.com>
2018-05-05 13:27:41 -0500

    Add a utility function to handle some of the logic in the reducers

--- redux/src/store/reducers/counter.js
+++ redux/src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   counter: 0
@@ -22,10 +23,8 @@ const reducer = (state = initialState, action) => {
         counter: state.counter + action.payload
       }
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.payload
-      }
+      // This produces the result as the ADD case above. It is simply using a utility function to handle the workload.
+      return updateObject(state, {counter: state.counter - action.payload});
     default:
       return state;
   }
