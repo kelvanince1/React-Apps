3fb4be254d092e11a76e81ec48c9e81fe20b7651
kelvanince1 <incekelvan@gmail.com>
2018-05-07 10:02:43 -0500

    Add a reducer to handle the purchasing success and fail for a burger order

--- burger-builder/src/store/actions/order.js
+++ burger-builder/src/store/actions/order.js
@@ -6,7 +6,7 @@ export const purchaseBurgerSuccess = (id, orderData) => {
   return {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
     orderId: id,
-    payload: orderData
+    orderData: orderData
   };
 };
 
