d7526e2d4375e9185087744aa5f1db5f6a06c887
kelvanince1 <incekelvan@gmail.com>
2018-05-08 09:07:59 -0500

    Add Auth file and use form to render controls to component screen

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './components/Layout/Layout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
         </Layout>
       </Route>
     );
