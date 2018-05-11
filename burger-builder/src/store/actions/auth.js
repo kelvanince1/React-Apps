fd4570b0e74d448caf4c77abb7a132c48cfe8b68
kelvanince1 <incekelvan@gmail.com>
2018-05-11 09:11:56 -0500

    Add a logout link and fix typo in auth action

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -4,61 +4,60 @@ import * as actionTypes from './actionTypes';
 import { SIGN_UP, SIGN_IN } from './axiosUrl';
 
 export const authStart = () => {
-  return {
-    type: actionTypes.AUTH_START
-  };
+    return {
+        type: actionTypes.AUTH_START
+    };
 };
 
 export const authSuccess = (token, userId) => {
-  return {
-    type: actionTypes.AUTH_SUCCESS,
-    idToken: token,
-    userId: userId
-  };
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
 };
 
 export const authFail = (error) => {
-  return {
-    type: actionTypes.AUTH_FAIL,
-    error: error
-  };
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
 };
 
 export const logout = () => {
-  return {
-    type: actionTypes.AUTH_LOGOUT
-  }
-}
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
 
 export const checkAuthTimeout = (expirationTime) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
-  }
-}
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
 
-export const auth = (email, password, isSignUp) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    };
+        email: email,
+        password: password,
+        returnSecureToken: true
+      };
     let url = SIGN_UP;
-    if (!isSignUp) {
+    if (!isSignup) {
       url = SIGN_IN
     }
     axios.post(url, authData)
       .then(response => {
-        console.log(response);
-        dispatch(authSuccess(response.data.idToken, response.data.localId))
-        dispatch(checkAuthTimeout(response.data.expiresIn))
+          console.log(response);
+          dispatch(authSuccess(response.data.idToken, response.data.localId));
+          dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
-        console.log(err);
-        dispatch(authFail(err.response.data.error));
-      })
-  };
+          dispatch(authFail(err.response.data.error));
+      });
+      };
 };
