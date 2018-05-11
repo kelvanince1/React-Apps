6f5c6b48a635f36ced538aa171e744b5af3b0acd
kelvanince1 <incekelvan@gmail.com>
2018-05-11 16:02:29 -0500

    Add routing guards

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -15,17 +15,37 @@ class App extends Component {
     this.props.onTryAutoSignUp();
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+        </Switch>
+      )
+    }
     return (
       <Route>
         <Layout>
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
+          {routes}
         </Layout>
       </Route>
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.authReducer.token !== null
   }
 }
 
@@ -35,4 +55,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
