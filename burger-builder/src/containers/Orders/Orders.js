3e27f9b62491233ac8e589154bf7d7fb52b2db88
kelvanince1 <incekelvan@gmail.com>
2018-05-11 08:00:48 -0500

    Pass token along with new order

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
   componentDidMount() {
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
 
   render() {
@@ -33,14 +33,15 @@ class Orders extends Component {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onFetchOrders: () => dispatch(actions.fetchOrders())
+    onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
   };
 };
 
 const mapStateToProps = (state) => {
   return {
     orders: state.orderReducer.orders,
-    loading: state.orderReducer.loading
+    loading: state.orderReducer.loading,
+    token: state.authReducer.token
   };
 };
 
