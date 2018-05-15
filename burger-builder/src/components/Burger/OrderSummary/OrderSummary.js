1eb56fe1c390ecb81e853d16155d300660595062
kelvanince1 <incekelvan@gmail.com>
2018-05-15 14:38:23 -0500

    Remove console logs

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,9 +4,6 @@ import Aux from '../../../hoc/Auxillery';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-  componentWillUpdate() {
-    console.log('Order summary will update')
-  }
   render() {
     const ingredientSummary = Object.keys(this.props.ingredients)
         .map( igKey => {
