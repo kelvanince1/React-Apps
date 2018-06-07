28e4792b55fdfa812d3e7704e61ad980fab7ded7
kelvanince1 <incekelvan@gmail.com>
2018-06-07 09:59:40 -0500

    Add link button to navigate between pages

--- server-side-rendering/.next/static/commons/manifest.js
+++ server-side-rendering/.next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "5d6a6c65c6b793749c81"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "34961cfd37e40a70d316"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js","5":"bundles\\pages\\auth.js"}[chunkId]||chunkId) + "-" + {"0":"a0e1f6e12aed407d18ef","1":"03948520cf8fa9fb98fb","2":"78938a05305dc136adc3","5":"96cbc4b9aaf00ab15530"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles\\pages\\_error.js","2":"bundles\\pages\\_app.js","4":"bundles\\pages\\index.js","5":"bundles\\pages\\auth.js"}[chunkId]||chunkId) + "-" + {"0":"a0e1f6e12aed407d18ef","1":"03948520cf8fa9fb98fb","2":"78938a05305dc136adc3","4":"e17ce65a3fe324138794","5":"96cbc4b9aaf00ab15530"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
