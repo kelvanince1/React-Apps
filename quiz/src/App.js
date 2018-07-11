ed97e6db697d881469ae214057f7d2667fd7abda
kelvanince1 <incekelvan@gmail.com>
2018-07-11 15:07:17 -0500

    Add questions and update state as correct answer is selected

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <Questionairre />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
--- quiz/src/components/Questionairre.js
+++ quiz/src/components/Questionairre.js
@@ -3,18 +3,45 @@ import React, { Component } from 'react';
 import questions from './questions.json';
 
 class Questionairre extends Component {
+  state = {
+    totalScore: 0,
+    usersSelection: null
+  };
+
+  handleClick = (item) => {
+    for (let i = 0; i < questions.results.length; i++) {
+      if (item === questions.results[i].correct_answer) {
+        this.setState(prevState => {
+          return {totalScore: prevState.totalScore + 1}
+        })
+      }
+    }
+
+    // for (let i = 0; i < questions.results; i++) {
+    //   console.log(questions.results[i].correct_answer);
+    //   if (item === questions.results[i].correct_answer) {
+    //     console.log('Correct');
+    //   }
+    // }
+  };
+
   render() {
     let result = questions.results.map((item,i) => {
       return (
-          <p key={i}>
-            {item.question}<br />
-            {item.incorrect_answers}
-            {item.correct_answer}
-          </p>
+          <div key={i}>
+            <p>{item.question}</p>
+            <ul style={{ 'listStyleType': 'none' }}>
+              <li onClick={() => this.handleClick(item.incorrect_answers[0].one)}>{item.incorrect_answers[0].one}</li>
+              <li onClick={() => this.handleClick(item.incorrect_answers[0].two)}>{item.incorrect_answers[0].two}</li>
+              <li onClick={() => this.handleClick(item.incorrect_answers[0].three)}>{item.incorrect_answers[0].three}</li>
+              <li onClick={() => this.handleClick(item.incorrect_answers[0].four)}>{item.incorrect_answers[0].four}</li>
+            </ul>
+          </div>
       );
     });
     return (
       <div>
+        <h4>Your score: {this.state.totalScore}</h4>
         {result}
       </div>
     );
