29c0abc4e5d726913a18a5fc28e7660dfe0d1866
kelvanince1 <incekelvan@gmail.com>
2018-07-31 15:22:25 -0500

    Add material ui Toolbar and AppBar to header component

--- venue/src/App.js
+++ venue/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './components/header_footer/Header';
+import './resources/styles.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <Header />
       </div>
     );
   }
