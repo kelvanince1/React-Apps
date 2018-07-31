dcf8e4dee2ef93b92cb53cdb3ca414549b63fc53
kelvanince1 <incekelvan@gmail.com>
2018-07-31 16:12:46 -0500

    Add event listener on componentDidMount to dynamically change a background color on user scroll

--- venue/src/components/header_footer/Header.js
+++ venue/src/components/header_footer/Header.js
@@ -9,7 +9,24 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
   state = {
-    drawerOpen: false
+    drawerOpen: false,
+    headerShow: false
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  };
+
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow: true
+      })
+    } else {
+      this.setState({
+        headerShow: false
+      })
+    }
   };
 
   toggleDrawer = (value) => {
@@ -22,7 +39,7 @@ class Header extends Component {
     return(
       <AppBar
         position="fixed"
-        style={{ backgroundColor: "#2f2f2f", boxShadow: 'none', padding: '10px 0px' }}
+        style={{ backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent', boxShadow: 'none', padding: '10px 0px' }}
       >
         <Toolbar>
           <div className="header_logo">
