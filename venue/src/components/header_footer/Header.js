22e0b7ca9a54b0419abb6edadb6a390e853d800a
kelvanince1 <incekelvan@gmail.com>
2018-07-31 15:26:59 -0500

    Add Icon button to Toolbar

--- venue/src/components/header_footer/Header.js
+++ venue/src/components/header_footer/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
@@ -17,6 +17,14 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
+
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => console.log('Opening')}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     );
