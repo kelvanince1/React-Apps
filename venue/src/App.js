6d2820b4abbadd7a89d74f959fc7d921c51b2fba
kelvanince1 <incekelvan@gmail.com>
2018-07-31 16:40:43 -0500

    Add transitioning slider to update image as user scrolls across the x axis

--- venue/src/App.js
+++ venue/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 import './resources/styles.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
         <Header />
+        <Featured />
       </div>
     );
   }
