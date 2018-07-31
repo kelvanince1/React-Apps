36774d1e3bb64b96e72eb632aece1a0da700215a
kelvanince1 <incekelvan@gmail.com>
2018-07-31 15:39:23 -0500

    Add side drawer and the props to handle its opening and closing

--- venue/src/components/header_footer/Header.js
+++ venue/src/components/header_footer/Header.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
+
+  state = {
+    drawerOpen: false
+  };
+
+  toggleDrawer = (value) => {
+    this.setState({
+      drawerOpen: value
+    })
+  }
+
   render() {
     return(
       <AppBar
@@ -21,10 +33,15 @@ class Header extends Component {
           <IconButton
             aria-label="Menu"
             color="inherit"
-            onClick={() => console.log('Opening')}
+            onClick={() => this.toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
+
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={(value) => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
