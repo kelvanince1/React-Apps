4fe6f483e78eb289dbcd737bd39ba19ab54e4d83
Kelvan Ince <incekelvan@gmail.com>
2018-08-01 18:24:17 -0500

    Add Footer and its icons

--- man_city/src/Components/Header_Footer/Footer.js
+++ man_city/src/Components/Header_Footer/Footer.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        Footer
+import { CityLogo } from '../UI/icons';
+
+const Footer = () => {
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo
+          width='70px'
+          height='70px'
+          link={true}
+          linkTo="/"
+         />
       </div>
-    );
-  };
-}
+      <div className="footer_discl">
+        Manchester City 2018. All rights reserved.
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- man_city/src/Components/Header_Footer/Header.js
+++ man_city/src/Components/Header_Footer/Header.js
@@ -4,7 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
-
+import { CityLogo } from '../UI/icons';
 
 class Header extends Component {
   render() {
@@ -13,10 +13,15 @@ class Header extends Component {
         position="fixed"
         style={{ backgroundColor: '#98c5e9', boxShadow: 'none', padding: '10px 0', borderBottom: '2px solid #00285e' }}
       >
-        <Toolbar styles={{ display: 'flex' }}>
-          <div styles={{ flexGrow: 1 }}>
+        <Toolbar style={{ display: 'flex' }}>
+          <div style={{ flexGrow: 1 }}>
             <div className="header_logo">
-              LOGO
+              <CityLogo
+                link={true}
+                linkTo="/"
+                width="70px"
+                height="70px"
+              />
             </div>
           </div>
 
