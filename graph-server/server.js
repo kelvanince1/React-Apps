0abb56825e83be100ece6ba157af3374544ea23c
Kelvan Ince <kelvan@mirafinancial.com>
2018-09-12 10:15:47 -0400

    Simple GraphQL server

---
+++ graph-server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { makeExecutableSchema } = require('graphql-tools');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+const port = 9000;
+
+const typeDefs = `
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello World!'
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers
+});
+
+const app = express();
+app.use(cors(), bodyParser.json());
+app.use('/graphql', graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+
+app.listen(port, () => console.log('Server running'));
