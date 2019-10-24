262d3789b1485db33ecc6afc9eefa7425fca396a
Kelvan Ince <incekelvan@gmail.com>
2019-10-24 14:34:45 -0400

    Add eslint packages

--- bundler-project/webpack.config.js
+++ bundler-project/webpack.config.js
@@ -9,5 +9,22 @@ module.exports = {
     },
     devServer: {
         contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // For any files which are js or jsx
+                exclude: /node_modules/,
+                use: ['babel-loader'] // Load them with Babel
+            },
+            {
+                test: /\.(js|jsx)$/, // For any files which are js or jsx
+                exclude: /node_modules/,
+                use: ['eslint-loader'] // Debug with eslint
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'] // No need to include .js or .jsx suffixes to those files when importing
     }
 }
