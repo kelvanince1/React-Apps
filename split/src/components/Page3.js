66db9c56cfe2c3cf2859001104537a8b24de04f4
Kelvan Ince <incekelvan@gmail.com>
2019-10-27 17:25:21 -0400

    Add starter project

---
+++ split/src/components/Page3.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+
+const Page3 = (props) =>  {
+    const { onRouteChange } = props;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+
+            <button onClick={() => onRouteChange('page2')}>Page 2</button>
+            <button onClick={() => onRouteChange('page3')}>Page 3</button>
+        </div>
+    );
+}
+
+export default Page3;
