e59d203105c56fba50ac38b8bd4867675561e498
Kelvan Ince <incekelvan@gmail.com>
2019-10-30 10:02:30 -0400

    Add React lazy to handle dynamic imports

--- split/src/App.js
+++ split/src/App.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
-import AsyncComponent from './components/Async/Async';
+//import AsyncComponent from './components/Async/Async';
+// We can use a different form of code splitting, specifically with React.lazy, whereby the variables are only loaded in the render function
+
+const Page2Lazy = React.lazy(() => import('./components/Page2'));
+const Page3Lazy = React.lazy(() => import('./components/Page3'));
 
 class App extends Component {
   constructor() {
@@ -21,14 +25,31 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.route === 'page1') {
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //     const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+    //     return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    // } else {
+    //     const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+    //     return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    // }
+
+    // Same result as above ^^ using React.lazy()
+     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
     } else if (this.state.route === 'page2') {
-        const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
-        return <AsyncPage2 onRouteChange={this.onRouteChange} />
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Page2Lazy onRouteChange={this.onRouteChange} />
+          </Suspense>
+        );
     } else {
-        const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
-        return <AsyncPage3 onRouteChange={this.onRouteChange} />
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Page3Lazy onRouteChange={this.onRouteChange} />
+          </Suspense>
+        );
     }
   }
 }
