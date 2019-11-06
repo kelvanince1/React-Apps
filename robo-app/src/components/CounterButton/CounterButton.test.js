ae5eb49934c16992ce00e66e9912e576a03a98bc
Kelvan Ince <incekelvan@gmail.com>
2019-11-06 11:21:57 -0500

    Add test to check props value is the expected one

--- robo-app/src/components/CounterButton/CounterButton.test.js
+++ robo-app/src/components/CounterButton/CounterButton.test.js
@@ -13,4 +13,5 @@ it('Correctly increments the counter', () => {
     wrapper.find('[id="counter"]').simulate('click');
     wrapper.find('[id="counter"]').simulate('click');
     expect(wrapper.state()).toEqual({ count: 2 });
+    expect(wrapper.props().color).toEqual('red');
 });
