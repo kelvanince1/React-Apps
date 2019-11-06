3d8e14671222683ac66ecee29723904c86bf86f2
Kelvan Ince <incekelvan@gmail.com>
2019-11-06 10:18:55 -0500

    Add snapshot test

---
+++ robo-app/src/components/Card/Card.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Card from './index';
+
+it('Expect to render Card component', () => {
+    expect(shallow(<Card />).toMatchSnapshot();
+});
