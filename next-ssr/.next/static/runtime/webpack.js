4ffc0478de0b804bd0f17db363ef8a66bcbebb2f
Kelvan Ince <incekelvan@gmail.com>
2019-11-25 10:11:36 -0500

    Add Next project

--- next-ssr/.next/static/runtime/webpack.js
+++ next-ssr/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "33ce6175575a6cd14db0";
+/******/ 	var hotCurrentHash = "f6dbd51adf7f1ea72096";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
