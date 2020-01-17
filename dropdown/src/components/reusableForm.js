7715b6df30c438ede803dc3223832b15ecee89ce
Kelvan Ince <incekelvan@gmail.com>
2020-01-17 14:19:24 -0500

    Rebuild the todo app from scratch

--- dropdown/src/components/reusableForm.js
+++ dropdown/src/components/reusableForm.js
@@ -1,27 +1,50 @@
+// import React, { useState } from 'react';
+
+// const useInputValue = (initialValue) => {
+//     const [value, setValue] = useState(initialValue);
+
+//     return {
+//         value,
+//         onChange: e => setValue(e.target.value),
+//         resetValue: () => setValue('')
+//     }
+// }
+
+// const ReusableForm = (props) => {
+//     const { resetValue, ...text } = useInputValue('');
+
+//     return (
+//         <form
+//             onSubmit={e => {
+//                 e.preventDefault();
+//                 props.onSubmit(text.value);
+//                 resetValue();
+//             }}
+//         >
+//              <input {...text} />
+//         </form>
+//     );
+// }
+
+// export default ReusableForm;
+
 import React, { useState } from 'react';
 
-const useInputValue = (initialValue) => {
-    const [value, setValue] = useState(initialValue);
-
-    return {
-        value,
-        onChange: e => setValue(e.target.value),
-        resetValue: () => setValue('')
-    }
-}
-
 const ReusableForm = (props) => {
-    const { resetValue, ...text } = useInputValue('');
+    const [value, setValue] = useState('');
 
     return (
         <form
-            onSubmit={e => {
+            onSubmit={(e) => {
                 e.preventDefault();
-                props.onSubmit(text.value);
-                resetValue();
+                props.onSubmit(value);
+                setValue('');
             }}
         >
-             <input {...text} />
+            <input 
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
         </form>
     );
 }
