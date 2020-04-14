276b363fe66b3a20279985da1eb362b7b0cc213d
Kelvan Ince <incekelvan@gmail.com>
2020-04-14 16:15:21 -0400

    Map todos and apply line through

--- use-reducer/src/todosReducer.js
+++ use-reducer/src/todosReducer.js
@@ -2,10 +2,12 @@ import React, { useReducer, useState } from 'react';
 
 const reducer = (state, action) => {
   switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
+    case 'ADD_TODO':
+      return { todos: [...state.todos, { text: action.payload, completed: false }] }
+    case 'TOGGLE_TODO':
+      return {
+          todos: state.todos.map((todo, i) => i === action.idx ? {...todo, completed: !todo.completed} : todo)
+      }
     default:
       return state;
   }
@@ -19,12 +21,25 @@ function Todos() {
     <div className="App">
       <form onSubmit={e => {
           e.preventDefault();
-          dispatch({ type: 'ADD_TODO', text: text })
+          dispatch({ type: 'ADD_TODO', payload: text });
+          setText('');
       }}>
         <input value={text} onChange={e => setText(e.target.value)} />
       </form>
       <div>
-          {JSON.stringify(todos)}
+          {
+              todos.map((todo, i) => (
+                  <div
+                    key={i}
+                    onClick={() => dispatch({ type: 'TOGGLE_TODO', idx: i })}
+                    style={{
+                        textDecoration: todo.completed ? 'line-through' : ''
+                    }}
+                >
+                    {todo.text}
+                </div>
+              ))
+          }
       </div>
     </div>
   );
