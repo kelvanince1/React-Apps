dd846e06ba1d409e790488550f7c2f13c9ea0b1f
Kelvan Ince <incekelvan@gmail.com>
2020-05-03 15:12:13 -0400

    Apply height dynamically

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './app.css';
 import List from './List';
 
 function App() {
   const [flashcards, setFlashcards] = useState(samples);
+  
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10')
+      .then(res => res.json())
+      .then(data => {
+        setFlashcards(data.results.map((question, i) => {
+          const answer = decodeStr(question.correct_answer);
+          const options = [
+            ...question.incorrect_answers.map(a => decodeStr(a)),
+            answer
+          ];
+
+          return {
+            id: `${i}-${Date.now()}`,
+            question: decodeStr(question.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
+  }, []);
+
+  const decodeStr = (str) => {
+     const text = document.createElement('textarea');
+     text.innerHTML = str;
+
+     return text.value;
+  }
+
   return (
-    <div>
+    <div className="container">
       <List
         flashcards={flashcards}
       />
--- flashcards/src/Flashcard.js
+++ flashcards/src/Flashcard.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Flashcard = ({ flashcard }) => {
     const [flip, setFlip] = useState(false);
+    const [height, setHeight] = useState('initial');
+
+    const frontEle = useRef();
+    const backEle = useRef();
+
+    const setMaxHeight = () => {
+        const frontHeight = frontEle.current.getBoundingClientRect().height;
+        const backHeight = backEle.current.getBoundingClientRect().height;
+
+        setHeight(Math.max(frontHeight, backHeight, 100));
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options]);
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight);
+        return () => window.removeEventListener('resize', setMaxHeight);
+    }, []);
 
     return (
         <div
             className={`card ${flip ? 'flip' : ''}`}
             onClick={() => setFlip(!flip)}
+            style={{ height: height }}
         >
-            <div className="front">
+            <div className="front" ref={frontEle}>
                 {flashcard.question}
                 <div className="flashcard-options">
                     {
@@ -20,7 +38,7 @@ const Flashcard = ({ flashcard }) => {
                     }
                 </div>
             </div>
-            <div className="back">
+            <div className="back" ref={backEle}>
                 {flashcard.answer}
             </div>
         </div>
