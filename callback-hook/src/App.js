22c2d64269ae8d5469f859014c960fc1035a3ce7
Kelvan Ince <incekelvan@gmail.com>
2020-07-21 15:13:00 -0400

    Add callback hook to getItems

--- callback-hook/src/App.js
+++ callback-hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import List from './List';
 
@@ -6,9 +6,9 @@ export default function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
+  const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
-  }
+  }, [number]);
 
   const theme = {
     backgroundColor: dark ? '#333' : '#FFF',
