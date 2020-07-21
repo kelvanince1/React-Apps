8954f62f82325eec3c891c8c2c80c98c08918e70
Kelvan Ince <incekelvan@gmail.com>
2020-07-21 15:06:26 -0400

    Add App and List components

---
+++ callback-hook/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import List from './List';
+
+export default function App() {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useState(false);
+
+  const getItems = () => {
+    return [number, number + 1, number + 2];
+  }
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF' : '#333'
+  }
+
+  return (
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={e => setNumber(parseInt(e.target.value))}
+      />
+
+      <button
+        onClick={() => setDark(prevDark => !prevDark)}
+      >
+        Toggle Theme
+      </button>
+
+      <List
+        getItems={getItems}
+      />
+    </div>
+  )
+}
