f82c0df2d4e643c7d5d35f1505e55265b8457872
Kelvan Ince <incekelvan@gmail.com>
2020-07-22 14:24:12 -0400

    Add reducer hook to handle increment and decrement state updates

---
+++ reducer-hook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useReducer } from 'react';
+
+const actions = {
+  INCREMENT: 'increment',
+  DECEREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // const [count, setCount] = useState(0);
+
+  function increment() {
+    dispatch({ type: 'increment' });
+  }
+
+  function decrement() {
+    dispatch({ type: 'decrement' });
+  }
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  );
+}
+
+export default App;
