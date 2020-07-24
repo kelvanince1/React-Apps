9143966aa85d1f03eb08b2f54d38dd03e1728990
Kelvan Ince <incekelvan@gmail.com>
2020-07-24 09:20:57 -0400

    Add unmounting effect

--- hooks/src/components/TodoList.js
+++ hooks/src/components/TodoList.js
@@ -3,12 +3,26 @@ import React, { useState, useEffect } from 'react';
 export default function TodoList() {
     const [resourceType, setResourceType] = useState('posts');
     const [items, setItems] = useState([]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    }
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
             .then(res => res.json())
             .then(json => setItems(json))
     }, [resourceType]);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+        
+    }, []);
 
     return (
         <>
@@ -19,11 +33,13 @@ export default function TodoList() {
             </div>
             <h1>{resourceType}</h1>
 
-            {
+            {/* {
                 items.map(item => {
                     return <pre key={item}>{JSON.stringify(item)}</pre>
                 })
-            }
+            } */}
+
+            <div>{windowWidth}</div>
         </>
     );
 }
