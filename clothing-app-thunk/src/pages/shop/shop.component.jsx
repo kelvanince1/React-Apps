3dd5dfb6f847e05b19df25aff09c25c5ba351811
Kelvan Ince <incekelvan@gmail.com>
2020-07-30 12:54:05 -0400

    Add Clothing store apps

---
+++ clothing-app-thunk/src/pages/shop/shop.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  componentDidMount() {
+    const { fetchCollectionsStartAsync } = this.props;
+
+    fetchCollectionsStartAsync();
+  }
+
+  render() {
+    const { match, isColletionFetching } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={isColletionFetching} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={isColletionFetching} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = createStructuredSelector({
+  isColletionFetching: selectIsCollectionFetching
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShopPage);
