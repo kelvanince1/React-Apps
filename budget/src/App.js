6652840f866a56746f423896d9bd8699a9f52fc9
Kelvan Ince <incekelvan@gmail.com>
2022-02-21 08:13:42 -0800

    Structure update

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,22 +1,27 @@
 import { Stack, Container, Button } from 'react-bootstrap';
+import AddBudgetModal from './components/AddBudgetModal';
 import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">
-          Budgets
-        </h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '1rem', alignItems: 'flex-start' }}>
-        <BudgetCard gray name="Entertainment" amount={200} max={1000}>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">
+            Budgets
+          </h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '1rem', alignItems: 'flex-start' }}>
+          <BudgetCard gray name="Entertainment" amount={200} max={1000}>
 
-        </BudgetCard>
-      </div>
-    </Container>
+          </BudgetCard>
+        </div>
+      </Container>
+
+      <AddBudgetModal show />
+    </>
   );
 }
 
--- budget/src/components/AddBudgetModal.js
+++ budget/src/components/AddBudgetModal.js
@@ -1,7 +1,36 @@
-export default function AddBudgetModal() {
+import { Modal, Button } from 'bootstrap';
+import { Form } from 'react-bootstrap';
+
+export default function AddBudgetModal({ show, handleClose }) {
+    function handleSubmit(e) {}
+
     return (
         <div>
+            <Modal show={show} onHide={handleClose}>
+                <Form onSubmit={handleSubmit}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>New Budget</Modal.Title>
+                    </Modal.Header>
+                    
+                    <Modal.Body>
+                        <Form.Group className="mb-3" controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" required />
+                        </Form.Group>
 
+                        <Form.Group className="mb-3" controlId="max">
+                            <Form.Label>Max Spend</Form.Label>
+                            <Form.Control type="number" required min={0} step={0.1} />
+                        </Form.Group>
+
+                        <div className="d-flex justify-content-end">
+                            <Button variant="primary" type="submit">
+                                Add
+                            </Button>
+                        </div>
+                    </Modal.Body>
+                </Form>
+            </Modal>
         </div>
     )
 }
